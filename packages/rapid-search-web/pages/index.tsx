e48b371f5f61ac5a178adb25ce93df6ecf5b38d0
Alex Goodman <alexthegoodman@gmail.com>
2022-08-01 18:11:06 -0400

    init web client

---
+++ packages/rapid-search-web/pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.scss";
+
+const Home: NextPage = () => {
+  return (
+    <main>
+      <section>RapidSearch</section>
+      <section></section>
+    </main>
+  );
+};
+
+export default Home;
