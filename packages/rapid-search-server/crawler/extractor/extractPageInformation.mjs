8817b7dd7044a80fde0d972a9309491d2e9c9a4f
Alex Goodman <alexthegoodman@gmail.com>
2022-11-23 13:36:58 -0500

    trim excerpts

--- packages/rapid-search-server/crawler/extractor/extractPageInformation.mjs
+++ packages/rapid-search-server/crawler/extractor/extractPageInformation.mjs
@@ -7,7 +7,7 @@ export const extractPageInformation = ($) => {
   const descriptionContent = $(`meta[name="description"]`).attr("content");
   const headlineText = $(`body h1`).text();
   const bodyText = $(`body p`).text();
-  const excerpt = bodyText.substr(0, excerptLength);
+  const excerpt = bodyText.substr(0, excerptLength).trim();
 
   console.info("Page Info ", titleContent, "description: ", descriptionContent);
   console.info("Body Info ", headlineText, "excerpt: ", excerpt);
