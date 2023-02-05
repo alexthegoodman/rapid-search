53f6b79d75b10a64d2dc4a23c699833bc7a80085
Alex Goodman <alexthegoodman@gmail.com>
2023-02-05 10:47:28 -0500

    cors

--- packages/scoring-service/application.py
+++ packages/scoring-service/application.py
@@ -7,8 +7,12 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_restful import Api, Resource
 from resources import GeneratorResource
+from flask_cors import CORS
 
 app = Flask(__name__)
+
+CORS(app)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
